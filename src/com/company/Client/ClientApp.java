ae9dc0c928dc8cbb58e73dcb6e30f4d751c6b596
Francisco Estrella <fran.estre@live.com>
2021-09-28 01:38:02 +0300

    plantilla del proyecto

---
+++ src/com/company/Client/ClientApp.java
@@ -0,0 +1,9 @@
+package com.company.Client;
+
+public class ClientApp {
+
+    public static void main(String[] args) {
+
+	// write your code here
+    }
+}
---
+++ src/com/company/Client/Communication.java
@@ -0,0 +1,18 @@
+package com.company.Client;
+
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Communication {
+    private final Integer port;
+    private final DatagramSocket datagramSocket;
+    private final InetAddress serverAddress;
+    public Communication(String serverAddress, Integer port) throws UnknownHostException, SocketException {
+        this.port = port;
+        datagramSocket = new DatagramSocket(this.port);
+        this.serverAddress = InetAddress.getByName(serverAddress);
+    }
+
+}
