f505bf0d5420804e2050094bd1d612b12ebe83c9
fran-estre <fran.estre@live.com>
2021-10-11 07:12:05 +0300

    cambios

--- src/com/company/Server/KeyboardHandler.java
+++ src/com/company/Server/KeyboardHandler.java
@@ -1,5 +1,7 @@
 package com.company.Server;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import java.util.Scanner;
 
 public class KeyboardHandler implements Runnable {
@@ -7,9 +9,14 @@ public class KeyboardHandler implements Runnable {
         String data;
         Scanner scanner = new Scanner(System.in);
         System.out.println("SAVE: to save file\nEXIT: to finish execution");
-        ProcessHandler processHandler = new ProcessHandler();
         while (!(data = scanner.nextLine().toUpperCase()).equals("EXIT")) {
             if (data.equals("SAVE")) {
+                ExecuteSave executeSave = new ExecuteSave();
+                try {
+                    executeSave.execute(null);
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("Saving file...");
                 System.out.println("SAVE: to save data\nEXIT: to finish execution");
             }
